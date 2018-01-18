2ad233ca9fb9b0bbf76183519bf0cbf75b666a1d
Maxime Clerix <mclerix@redhat.com>
2018-01-18 14:18:06 +0100

    Update create-nfs-share.sh

--- persistent-volume/create-nfs-share.sh
+++ persistent-volume/create-nfs-share.sh
@@ -5,7 +5,7 @@ do
 pv=pv$vol   
 mkdir -p /exports/$pv
 chown -R nfsnobody:nfsnobody /exports/$pv 
-chmod -R 777 /exports/$pv
+chmod -R 755 /exports/$pv
 echo "/exports/$pv *(rw,root_squash)" >> /etc/exports.d/openshift-userpvs.exports
 done
 
